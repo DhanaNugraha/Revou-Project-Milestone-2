ce85f812df614a73014a7495513fe1b75b042266
DhanaNugraha <dhananugraha1511@gmail.com>
2025-01-21 16:22:39 +0700

    feat: made form component

---
+++ module5assignment/src/components/LoginRegisterForm.tsx
@@ -0,0 +1,23 @@
+const LoginRegisterForm = ({email, setEmail, password, setPassword, onSubmit, formType}:any) => {
+    const submitButtonStyling = "border text-[1em] font-medium bg-[#242424] cursor-pointer transition-[border-color] duration-[0.25s] px-[1.2em] py-[0.6em] rounded-lg border-solid border-transparent hover:border-[#646cff]"
+
+  return (
+        <form onSubmit={onSubmit} className={"flex flex-col place-items-center p-[10%]"}>
+            <fieldset className="formFill email">
+                <label htmlFor="email">Email </label>
+                <input type="email" name="email" id="email" value={email} onChange={(event) => {setEmail(event.target.value)}} />
+            </fieldset>
+
+            <fieldset className="formFill password">
+                <label htmlFor="password">Password </label>
+                <input type="password" name="password" id="password" value={password} onChange={(event) => {setPassword(event.target.value)}}/>
+            </fieldset>
+
+            <fieldset className={submitButtonStyling}>
+            <button type="submit">{formType}</button>
+            </fieldset>
+        </form>
+    )
+}
+
+export default LoginRegisterForm;
--- module5assignment/src/pages/login/index.tsx
+++ module5assignment/src/pages/login/index.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
+import LoginRegisterForm from "@/components/LoginRegisterForm";
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const router = useRouter();
-
-    const submitButtonStyling = "border text-[1em] font-medium bg-[#1a1a1a] cursor-pointer transition-[border-color] duration-[0.25s] px-[1.2em] py-[0.6em] rounded-lg border-solid border-transparent hover:border-[#646cff]"
 
     const handleLoginSubmit = async (event: any) => {
         event.preventDefault();
@@ -44,21 +43,14 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={handleLoginSubmit} className={"flex flex-col place-items-center p-[10%]"}>
-            <fieldset className="formFill email">
-                <label htmlFor="email">Email </label>
-                <input type="email" name="email" id="email" value={email} onChange={(event) => {setEmail(event.target.value)}} />
-            </fieldset>
-
-            <fieldset className="formFill password">
-                <label htmlFor="password">Password </label>
-                <input type="password" name="password" id="password" value={password} onChange={(event) => {setPassword(event.target.value)}}/>
-            </fieldset>
-
-            <fieldset className={submitButtonStyling}>
-            <button type="submit">Login</button>
-            </fieldset>
-        </form>
+        <LoginRegisterForm
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            onSubmit={handleLoginSubmit}
+            formType = "Login"
+        />
     );
 };
 
